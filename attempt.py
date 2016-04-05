438cb97291938fd53dc7d0e3a4274a8043fee6dc
Megan Ghastin <mghastin@gmail.com>
2016-04-04 22:14:04 -0400

    Added Progress class to track Attempts

--- attempt.py
+++ attempt.py
@@ -1,9 +1,35 @@
+import string 
 class Attempt(object):
 	def __init__(self, score, outof, date, lg, comment):
 		# store values in a dictionary called "info"
 		self.info = {"score": score, "outof": outof, 
 				   "date": date, "lg": lg, "comment": comment}
-				   	
+		self.month, self.day, self.year = self.__parse_date__()
+	
+	def __parse_date__(self):
+		# date must be in mm/dd/yyyy format
+		month, day, year = string.split(self.info["date"], "/")
+		return int(month), int(day), int(year)
+	
+	# define comparison function so can sort attempts by date!
+	def __cmp__(self, other): # return -1 if self less, 0 if same, 1 if self more
+		if self.year < other.year:
+			return -1
+		elif self.year == other.year: # if same year, compare months
+			if self.month < other.month: 
+				return -1
+			elif self.month == other.month: # if same month, compare days
+				if self.day < other.day:
+					return -1
+				elif self.day == other.day: # same month, day, yr -> equal!
+					return 0
+				else:
+					return 1
+			else:
+				return 1
+		else:
+			return 1			
+					   	
 	def get_score(self):
 		return self.info["score"]
 	
---
+++ progress.py
@@ -0,0 +1,35 @@
+from learning_goal import LearningGoal
+from attempt import Attempt
+
+class Progress(object):
+	
+	def __init__(self, learning_goals, type): # give list of LearningGoals
+		assert isinstance(learning_goals, list), "Not a list of Learning Goals" 
+		self.attempts = {}
+		for lg in learning_goals:
+			assert isinstance(lg, LearningGoal), "Not Learning Goal"
+			self.attempts[lg] = [] # make an empty list for each lg
+		self.type = type
+
+	def get_type(self):
+		return self.type
+	
+	def get_attempts(self,lg): # need to pass a LearningGoal object
+		assert isinstance(lg, LearningGoal), "Not Learning Goal"
+		return self.attempts[lg] # returns a list of Attempts
+	
+	def add_attempt(self, attempt, lg): 
+		assert isinstance(attempt, Attempt), "Not Attempt"
+		assert isinstance(lg, LearningGoal), "Not Learning Goal"
+		assert lg in self.attempts, "Progress does not track this Learning Goal."
+		self.attempts[lg].append(attempt)
+		self.attempts[lg].sort(reverse = True) # sort newest first
+
+	def most_recent(self, lg): # pass a Learning Goal, gives most recent attempt
+		assert type(lg) is LearningGoal, "Not a Learning Goal."
+		assert lg in self.attempts, "Progress does not track this Learning Goal."
+		return self.attempts[lg][0]
+	
+	# for later: make attempt search function
+	# for later: make attempt delete function
+	# store the best attempt for each learning goal (highest!!)
